27ca4763660af4b5e3bf1c1223782d7c4ea3d2af
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-03-06 14:23:46 +0200

    ex2

---
+++ lab2/src/Socaci/Cristi/lab2/ex2/Main.java
@@ -0,0 +1,14 @@
+package Socaci.Cristi.lab2.ex2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String[] s = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+        System.out.println("Enter a number from 1 to 9: ");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println(s[a-1]);
+    }
+}
