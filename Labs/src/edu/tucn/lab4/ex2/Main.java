5d7230821fa3f252729199d89f3c51b70a6b248e
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-03-18 18:07:04 +0200

    ex2

---
+++ Labs/src/edu/tucn/lab4/ex2/Main.java
@@ -0,0 +1,10 @@
+package edu.tucn.lab4.ex2;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Author a = new Author("Cristi", "dadada@dada.da", 'm');
+        System.out.println(a.toString());
+    }
+}
