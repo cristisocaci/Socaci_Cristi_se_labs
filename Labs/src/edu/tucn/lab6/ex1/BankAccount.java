791d01e3239832ef9401f9e5277b6d7978166621
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-04-03 12:27:37 +0300

    ex1

---
+++ Labs/src/edu/tucn/lab6/ex1/BankAccount.java
@@ -0,0 +1,44 @@
+package edu.tucn.lab6.ex1;
+
+import java.util.Objects;
+
+public class BankAccount {
+    private String owner;
+    private double  balance;
+
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankAccount that = (BankAccount) o;
+        return Double.compare(that.balance, balance) == 0 &&
+                Objects.equals(owner, that.owner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(owner, balance);
+    }
+
+    public void withdraw(double amount){
+        if(amount < 0)
+            System.out.println("Enter a valid amount");
+        else if(balance - amount < 0)
+            System.out.println("Insufficient funds");
+        else
+            balance -= amount;
+    }
+
+    public void deposit(double amount){
+        if(amount < 0)
+            System.out.println("Enter a valid amount");
+        else
+            balance += amount;
+    }
+
+}
--- Labs/src/edu/tucn/lab6/ex1/Main.java
+++ Labs/src/edu/tucn/lab6/ex1/Main.java
@@ -2,6 +2,9 @@ package edu.tucn.lab6.ex1;
 
 public class Main {
     public static void main(String[] args) {
+        BankAccount b = new BankAccount("Cristi", 2000);
+        BankAccount c = new BankAccount("Cristi", 2000);
 
+        System.out.println(c.equals(b));
     }
 }
